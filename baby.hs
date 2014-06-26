367225ff181f30a05a13e3afa74592f4dfb07ab7
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-26 13:26:13 +1000

    Added functiond first, second, third

--- baby.hs
+++ baby.hs
@@ -38,3 +38,9 @@ charName 'c' = "Cecil"
 charName x = "No character name found!"
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+first :: (a, b, c) -> a
+first (x, _, _) = x
+second :: (a, b, c) -> b
+second (_, y, _) = y
+third :: (a, b, c) -> c
+third (_, _, z) = z
