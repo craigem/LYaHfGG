93bf8a039d9af617adae71fcfab69a942c5b4aa1
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-26 13:03:56 +1000

    Added type lucky

--- baby.hs
+++ baby.hs
@@ -17,3 +17,6 @@ circumference :: Float -> Float
 circumference r = 2 * pi * r
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
