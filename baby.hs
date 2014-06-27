f50ab9e363684346883a3ec601b8176edf2827d7
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 13:50:59 +1000

    Added function initials

--- baby.hs
+++ baby.hs
@@ -91,17 +91,14 @@ a `myCompare` b
 	| otherwise = LT
 
 -- Where!?
-bmiTell :: (RealFloat a) => a -> a -> String
-bmiTell weight height
-	| bmi <= skinny = "You're underweight, you emo, you!"
-	| bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
-	| bmi <= fat = "You're fat! Lose some weigh, fatty!"
-	| otherwise   = "You're a whale, congratulations!"
-	where bmi = weight / height ^ 2 
+-- bmiTell :: (RealFloat a) => a -> a -> String
+-- bmiTell weight height
+-- 	| bmi <= skinny = "You're underweight, you emo, you!"
+-- 	| bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+-- 	| bmi <= fat = "You're fat! Lose some weigh, fatty!"
+-- 	| otherwise   = "You're a whale, congratulations!"
+-- 	where bmi = weight / height ^ 2
 --          (skinny, normal, fat) = (18.5, 25.0, 30.0) {- this line is broken -}
-		skinny = 18.5  
-		normal = 25.0  
-		fat = 30.0  
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
