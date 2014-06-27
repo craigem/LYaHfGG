cafa0e323a836a6d1be50e54100b8e818f4893aa
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 11:02:13 +1000

    Re-wrote bmiTell with additional names

--- baby.hs
+++ baby.hs
@@ -93,8 +93,11 @@ a `myCompare` b
 -- Where!?
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
-	| bmi <= 18.5 = "You're underweight, you emo, you!"
-	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-	| bmi <= 30.0 = "You're fat! Lose some weigh, fatty!"
+	| bmi <= skinny = "You're underweight, you emo, you!"
+	| bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| bmi <= fat = "You're fat! Lose some weigh, fatty!"
 	| otherwise   = "You're a whale, congratulations!"
-	where bmi = weight / height ^ 2
+	where bmi = weight / height ^ 2 
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
