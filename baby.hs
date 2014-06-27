3f7b273f0765f8e75b17694962437e0201c6c9ee
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 14:04:42 +1000

    Added function calcBmis

--- baby.hs
+++ baby.hs
@@ -105,3 +105,13 @@ initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
 	where 
 		(f:_) = firstname
 		(l:_) = lastname
+
+{- calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+	where bmi weight height = weight / height ^ 2 -}
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs]  
+    where bmi weight height = weight / height ^ 2  
+
+-- let it be
