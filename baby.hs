8a3be96a9d954bb8ab0b45a318971c0b33751156
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 10:29:42 +1000

    Added function bmiTell

--- baby.hs
+++ baby.hs
@@ -65,3 +65,9 @@ sum' (x:xs) = x + sum' xs
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+	| bmi <= 18.5 = "You're underweight, you emo, you!"
+	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| bmi <= 30.0 = "You're fat! Lose some weigh, fatty!"
+	| otherwise   = "You're a whale, congratulations!"
