9b3ab58ac54d2e2984a220f0ad921c09e8c8fdbc
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 10:39:26 +1000

    Added function max'

--- baby.hs
+++ baby.hs
@@ -65,9 +65,14 @@ sum' (x:xs) = x + sum' xs
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+-- Guards! Guards!
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
 	| weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
 	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
 	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weigh, fatty!"
 	| otherwise   = "You're a whale, congratulations!"
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
