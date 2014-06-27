dc642488df140dfe1e51fb241ad947ee9ce2cc75
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-27 10:34:57 +1000

    Re-wrote bmiTell to calculate BMI too

--- baby.hs
+++ baby.hs
@@ -66,8 +66,8 @@ capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 bmiTell :: (RealFloat a) => a -> String
-bmiTell bmi
-	| bmi <= 18.5 = "You're underweight, you emo, you!"
-	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-	| bmi <= 30.0 = "You're fat! Lose some weigh, fatty!"
+bmiTell weight height
+	| weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weigh, fatty!"
 	| otherwise   = "You're a whale, congratulations!"
