670d738d1a40aa9cc0f274c76d63bc7c1ae4ca92
Craige McWhirter <craige@mcwhirter.com.au>
2014-06-30 10:53:32 +1000

    Added function cylindar

--- baby.hs
+++ baby.hs
@@ -91,14 +91,15 @@ a `myCompare` b
 	| otherwise = LT
 
 -- Where!?
--- bmiTell :: (RealFloat a) => a -> a -> String
--- bmiTell weight height
--- 	| bmi <= skinny = "You're underweight, you emo, you!"
--- 	| bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
--- 	| bmi <= fat = "You're fat! Lose some weigh, fatty!"
--- 	| otherwise   = "You're a whale, congratulations!"
--- 	where bmi = weight / height ^ 2
---          (skinny, normal, fat) = (18.5, 25.0, 30.0) {- this line is broken -}
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
@@ -106,12 +107,15 @@ initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
 		(f:_) = firstname
 		(l:_) = lastname
 
-{- calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis xs = [bmi w h | (w, h) <- xs]
-	where bmi weight height = weight / height ^ 2 -}
-
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
 calcBmis xs = [bmi w h | (w, h) <- xs]  
     where bmi weight height = weight / height ^ 2  
 
 -- let it be
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in sideArea + 2 * topArea
+
