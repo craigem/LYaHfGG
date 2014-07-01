a7c2fce2b92e39b7bcc1c23537292ea59948efae
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-01 13:27:30 +1000

    Added function compareWithHundred

--- baby.hs
+++ baby.hs
@@ -191,3 +191,6 @@ quicksort (x:xs) =
 
 multThree :: (Num a) => a -> a -> a -> a
 multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
