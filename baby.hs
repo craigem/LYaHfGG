a4d980f1ab0301b943057b73052d8b04e56cb79e
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-02 16:40:54 +1000

    Added function filter'

--- baby.hs
+++ baby.hs
@@ -219,4 +219,10 @@ flip' f =g
 
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
-map' f (x:xs) = f x : map f xs
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
