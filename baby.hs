db44b496d6fb8dffb7861951e664a0c00c0ccbf7
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-03 11:08:24 +1000

    Added function largestDivisible

--- baby.hs
+++ baby.hs
@@ -235,3 +235,7 @@ quicksort (x:xs) =
     let smallerSorted = quicksort (filter (<=x) xs)
         biggerSorted = quicksort (filter (>x) xs)
     in smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
