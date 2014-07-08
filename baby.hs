5252079f9fc841833aa2395aa3584f59c69adf04
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-08 11:18:45 +1000

    Added function search

--- baby.hs
+++ baby.hs
@@ -329,3 +329,10 @@ oddSquareSum =
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+--Data.List
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+    let nlen = length needle
+    in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
