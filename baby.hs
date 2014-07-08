6c44f5513146d67d06f16d7ee37a7bcec193d352
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-08 11:35:32 +1000

    Added function decode

--- baby.hs
+++ baby.hs
@@ -345,3 +345,6 @@ encode shift msg =
     let ords = map ord msg
         shifted = map (+ shift) ords
     in map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
