1086007a704fdd256e3efa33551b86fdbad72279
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-09 14:58:35 +1000

    Added function phoneBookToMap'

--- baby.hs
+++ baby.hs
@@ -355,11 +355,15 @@ decode shift msg = encode (negate shift) msg
 
 phoneBook =
     [("betty","555-2938")
-    ,("bonnie","453-2928")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
     ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
     ,("lucille","205-2928")
     ,("wendy","939-8282")
     ,("penny","853-2492")
+    ,("penny","555-2111")
     ]
 
 findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
@@ -367,3 +371,6 @@ findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
 
 fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
 fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
