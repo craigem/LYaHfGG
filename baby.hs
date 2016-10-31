1d689156788bd25ed8869c8581fc1628b39696a9
Craige McWhirter <craige@mcwhirter.io>
2016-10-31 11:50:31 +1000

    Revisited Syntax in Functions

--- baby.hs
+++ baby.hs
@@ -24,8 +24,9 @@ epicHilarity =
     , noun <- nouns
     ]
 
--- Replaced later
+{- Replaced later
 --length' xs = sum [1 | _ <- xs]
+-}
 
 triangles =
     [ (a,b,c) | c <- [1..10]
@@ -52,9 +53,10 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
--- Replaced later
---factorial :: Integer -> Integer
---factorial n = product [1..n]
+{- Replaced later
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+-}
 
 -- Float is a real floating point with single precision.
 circumference :: Float -> Float
@@ -69,6 +71,7 @@ circumference' r = 2 * pi * r
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
+
 sayMe :: (Integral a) => a -> String
 sayMe 1 = "One!"
 sayMe 2 = "Two!"
@@ -76,42 +79,51 @@ sayMe 3 = "Three!"
 sayMe 4 = "Four!"
 sayMe 5 = "Five!"
 sayMe x = "Not between 1 and 5"
+
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
 charName :: Char -> String
 charName 'a' = "Albert"
 charName 'b' = "Broseph"
 charName 'c' = "Cecil"
 charName x = "No character name found!"
+
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
 first :: (a, b, c) -> a
 first (x, _, _) = x
 second :: (a, b, c) -> b
 second (_, y, _) = y
 third :: (a, b, c) -> c
 third (_, _, z) = z
+
 {- re-written below
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
 -}
+
 tell :: (Show a) => [a] -> String
 tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
 -- Re-write length using pattern matching
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
 -- Re-write sum using patterns
 {- Re-written below
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
 -}
+
 -- Pattern shortcuts
 capital :: String -> String
 capital "" = "Empty string, whoops!"
@@ -119,13 +131,14 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 -- Guards, guards!
 
--- Re-written with where later
--- bmiTell :: (RealFloat a) => a -> a -> String
--- bmiTell weight height
--- 	| weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
--- 	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
--- 	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weigh, fatty!"
--- 	| otherwise   = "You're a whale, congratulations!"
+{- Re-written with where later
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+   	| weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+   	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+   	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weigh, fatty!"
+   	| otherwise   = "You're a whale, congratulations!"
+-}
 
 max' :: (Ord a) => a -> a -> a
 max' a b
@@ -134,9 +147,9 @@ max' a b
 
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
-	| a > b		= GT
-	| a == b	= EQ
-	| otherwise = LT
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
 
 -- Where!?
 
@@ -151,9 +164,9 @@ bmiTell weight height
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-	where
-		(f:_) = firstname
-		(l:_) = lastname
+    where
+        (f:_) = firstname
+        (l:_) = lastname
 
 {- Rewitten using let below
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
