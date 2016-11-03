fd5d00e7200fa6fea278f06185552c9b665e6d5c
Craige McWhirter <craige@mcwhirter.io>
2016-11-04 09:08:28 +1000

    Completed record Syntax

--- Chapter08.hs
+++ Chapter08.hs
@@ -7,6 +7,7 @@ module Shapes
     , nudge
     , baseCircle
     , baseRect
+    , Person(..)
     ) where
 
 -- Define Shape
@@ -46,3 +47,44 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+-- Record Syntax
+
+-- Create a data type that describes a person
+-- Stores:
+-- first name, surname, age, height, phone number, favorite ice-cream flavor.
+--data Person = Person String String Int Float String String deriving (Show)
+
+-- Create a function to get seperate info from a person?
+--firstName :: Person -> String
+--firstName (Person firstname _ _ _ _ _) = firstname
+
+--lastName  :: Person -> String
+--lastName (Person _ lastname _ _ _ _) = lastname
+
+--age :: Person -> Int
+--age (Person _ _ age _ _ _) = age
+
+--height :: Person -> Float
+--height (Person _ _ _ height _ _) = height
+
+--phoneNumber :: Person -> String
+--phoneNumber (Person _ _ _ _ number _) = number
+
+--flavor :: Person -> String
+--flavor (Person _ _ _ _ _ flavor) = flavor
+
+-- Re-write the above data type and records more efficiently.
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+-- define a car using record syntax
+data Car = Car { company :: String
+               , model :: String
+               , year :: Int
+               } deriving (Show)
