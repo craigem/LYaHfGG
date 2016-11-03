203d565ba6fc504f76b7a7e9377d75e3f4e41c52
Craige McWhirter <craige@mcwhirter.io>
2016-11-03 21:03:53 +1000

    Got a bit further along Making Our Own Types and Typeclasses

--- Chapter08.hs
+++ Chapter08.hs
@@ -1,5 +1,14 @@
 -- Chapter 8 - Making Our Own Types and Typeclasses
 
+module Shapes
+    ( Point(..)
+    , Shape(..)
+    , surface
+    , nudge
+    , baseCircle
+    , baseRect
+    ) where
+
 -- Define Shape
 --data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 
@@ -19,3 +28,21 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+-- a function that nudges a shape
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (
+    Rectangle (
+        Point x1 y1) (Point x2 y2)) a b =
+    Rectangle (
+        Point (x1+a) (y1+b)) (Point (x2+a) (y2+b)
+    )
+
+-- make some auxilliary functions that create shapes of some size at the zero
+-- coordinates and then nudge those
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
