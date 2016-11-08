ba02df9a13f886eed181e85f5178dfed6baaaec9
Craige McWhirter <craige@mcwhirter.io>
2016-11-08 14:51:58 +1000

    Completed Derived instances

--- Chapter08.hs
+++ Chapter08.hs
@@ -76,13 +76,14 @@ baseRect width height = Rectangle (Point 0 0) (Point width height)
 --flavor (Person _ _ _ _ _ flavor) = flavor
 
 -- Re-write the above data type and records more efficiently.
-data Person = Person { firstName :: String
-                     , lastName :: String
-                     , age :: Int
-                     , height :: Float
-                     , phoneNumber :: String
-                     , flavor :: String
-                     } deriving (Show)
+--data Person = Person { firstName :: String
+--                     , lastName :: String
+--                     , age :: Int
+--                     , height :: Float
+--                     , phoneNumber :: String
+--                     , flavor :: String
+--                     } deriving (Show)
+-- Re-written below
 
 -- define a car using record syntax
 data Car = Car { company :: String
@@ -111,3 +112,14 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `scalarMult` (Vector l m n) = i * l + j * m + k * n
+
+-- Derived instances
+
+data Person = Person
+    { firstName :: String
+    , lastName :: String
+    , age :: Int
+    } deriving (Eq, Show, Read)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
