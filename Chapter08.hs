ead705874ad9c8e8260094bd0100bf65c7d98c3f
Craige McWhirter <craige@mcwhirter.io>
2016-11-09 10:51:40 +1000

    Completed Type synonyms

--- Chapter08.hs
+++ Chapter08.hs
@@ -11,6 +11,8 @@ module Shapes
     , Maybe(..)
     ) where
 
+import qualified Data.Map as Map
+
 -- Define Shape
 --data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 
@@ -123,3 +125,54 @@ data Person = Person
 
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- Type synonyms
+
+phoneBook :: [(String,String)]
+phoneBook =
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+type AssocList k v = [(k,v)]
+
+
+type IntMap = Map.Map Int
+
+-- Use a map from Data.Map to represent the lockers. Map from locker numbers to
+-- a pair of whether the locker is in use or not and the locker code.
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+            then Right code
+            else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken, "ZD39I"))
+    ,(101,(Free, "JAH3I"))
+    ,(103,(Free, "IQSA9"))
+    ,(105,(Free, "QOTSA"))
+    ,(109,(Taken, "893JJ"))
+    ,(110,(Taken, "99292"))
+    ]
