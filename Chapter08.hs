3e43edba5e2637940b834aa74e29673840e58743
Craige McWhirter <craige@mcwhirter.io>
2016-11-10 08:43:40 +1000

    Completed Kinds and some type-foo and by extensionm Chapter 8 itself.

--- Chapter08.hs
+++ Chapter08.hs
@@ -281,8 +281,17 @@ instance Functor Tree where
     fmap f EmptyTree = EmptyTree
     fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
 
--- partially apply Either by feeding it only one parameter so that it has one
--- free parameter
-instance Functor (Either a) where
-    fmap f (Right x) = Right (f x)
-    fmap f (Left x) = Left x
+-- Kinds and some type-foo
+
+class Tofu t where
+    tofu :: j a -> t a j
+
+data Frank a b = Frank {frankField :: b a} deriving (Show)
+
+instance Tofu Frank where
+    tofu x = Frank x
+
+data Barry t k p = Barry { yabba :: p, dabba :: t k}
+
+instance Functor (Barry a b) where
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba =y}
