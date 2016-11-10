ce544b1b3abd934275497ff4972456984dd84403
Craige McWhirter <craige@mcwhirter.io>
2016-11-10 14:53:35 +1000

    Started Files and Streams, initial commit of capslocker

---
+++ capslocker.hs
@@ -0,0 +1,10 @@
+-- capslocker.hs from Chapter 09
+
+-- import required modules:
+import Control.Monad
+import Data.Char
+
+main = forever $ do
+    putStr "Give me some input: "
+    l <- getLine
+    putStrLn $ map toUpper l
