0c01acf64c0ea76f12145a3921664101e61e9ac1
Craige McWhirter <craige@mcwhirter.io>
2016-11-12 11:21:47 +1000

    re-written with withfile

--- girlfriend.hs
+++ girlfriend.hs
@@ -4,7 +4,6 @@
 import System.IO
 
 main = do
-    handle <- openFile "girlfriend.txt" ReadMode
-    contents <- hGetContents handle
-    putStr contents
-    hClose handle
+    withFile "girlfriend.txt" ReadMode (\handle -> do
+        contents <- hGetContents handle
+        putStr contents)
