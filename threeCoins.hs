e712f3dde11a3bfa8e79eb8e3b6d9865745c881d
Craige McWhirter <craige@mcwhirter.io>
2016-11-19 20:50:04 +1000

    Updated threeCoins

--- threeCoins.hs
+++ threeCoins.hs
@@ -1,2 +1,8 @@
 -- threeCoins from Chapter 09
 
+threeCoins :: StdGen -> (Bool. Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in  (firstCoin, secondCoin, thirdCoin)
