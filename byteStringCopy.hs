f6dcbef1722bde47d8e49d48dd414d08d6cbf600
Craige McWhirter <craige@mcwhirter.io>
2016-11-21 18:42:53 +1000

    Initial commit of byteStringCopy

---
+++ byteStringCopy.hs
@@ -0,0 +1,14 @@
+-- byteStringCopy from Chapter 09
+
+-- import required modules
+import System.Environment
+import qualified Data.ByteString.Lazy as B
+
+main = do
+    (fileName1:fileName2:_) <- getArgs
+    copyFile fileName1 fileName2
+
+copyFile :: FilePath -> FilePath -> IO ()
+copyFile source dest = do
+    contents <- B.readFile source
+    B.writeFile dest contents
