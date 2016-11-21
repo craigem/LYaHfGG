28899265a139806903f58040697f5a40cdd0fd48
Craige McWhirter <craige@mcwhirter.io>
2016-11-21 18:57:57 +1000

    Added exception message

--- linecount.hs
+++ linecount.hs
@@ -3,7 +3,12 @@
 -- import required modules
 import System.Environment
 import System.IO
+import System.Directory
 
 main = do (fileName:_) <- getArgs
-          contents <- readFile fileName
-          putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
+          -- Check the file provided actually exists
+          fileExists <- doesFileExist fileName
+          if fileExists
+              then do contents <- readFile fileName
+                      putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
+              else do putStrLn "The file doesn't!"
