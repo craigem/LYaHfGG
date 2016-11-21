3dae8be0af7d03bf5c3c3f9da2075ec50daa4ebe
Craige McWhirter <craige@mcwhirter.io>
2016-11-21 18:06:24 +1000

    Completed guess_the_number

--- guess_the_number.hs
+++ guess_the_number.hs
@@ -11,11 +11,18 @@ main = do
 askForNumber :: StdGen -> IO ()
 askForNumber gen = do
     let (randNumber, newGen) = randomR (1,10) gen :: (Int, StdGen)
+    -- Tell the user to guess the number we're thinking of
     putStr "Which number in the range from 1 to 10 am I thinking of? "
+    -- get input from stdin and assign to numberString
     numberString <- getLine
+    -- Use  when to check if the string the user entered is empty
     when (not $ null numberString) $ do
         let number = read numberString
+        -- Compare the input to the random number generated
         if randNumber == number
+            -- If they match, send this to stdout
             then putStrLn "You are correct!"
+            -- If they don't match, send this to stdout instead.
             else putStrLn $ "Sorry, it was " ++ show randNumber
+        -- Do it all again!
         askForNumber newGen
