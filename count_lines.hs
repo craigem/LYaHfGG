1f87398c341832bf9e1be256116cc91b5e5c095d
Craige McWhirter <craige@mcwhirter.io>
2016-11-22 16:58:06 +1000

    Completed count_lines

--- count_lines.hs
+++ count_lines.hs
@@ -5,12 +5,13 @@ import System.Environment
 import System.IO
 import System.IO.Error
 
-main = toTry catch handler
+-- Book says to use catch but it has been replaced with catchIOError
+main = toTry `catchIOError` handler
 
 toTry :: IO ()
 toTry = do (fileName:_) <- getArgs
-    contents <- readFile fileName
-    putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
+           contents <- readFile fileName
+           putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
 
 handler :: IOError -> IO ()
 handler e = putStrLn "Whoops, had some trouble!"
