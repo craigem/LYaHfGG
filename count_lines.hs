a0fb63f58cfe95a80c1bbf1d34ca136fccdafc27
Craige McWhirter <craige@mcwhirter.io>
2016-11-22 16:28:58 +1000

    Initial commit of count_lines

---
+++ count_lines.hs
@@ -0,0 +1,16 @@
+-- count_lines from Chapter 09
+
+-- import required modules
+import System.Environment
+import System.IO
+import System.IO.Error
+
+main = toTry catch handler
+
+toTry :: IO ()
+toTry = do (fileName:_) <- getArgs
+    contents <- readFile fileName
+    putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"
+
+handler :: IOError -> IO ()
+handler e = putStrLn "Whoops, had some trouble!"
